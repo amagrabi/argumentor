55e23b140f183487de492a694b7ecbb2d86826cd
amagrabi <amadeus.magrabi@gmail.com>
2025-02-05 15:35:30 +0100

    Factor out llm eval to separate file

--- src/app.py
+++ src/app.py
@@ -1,4 +1,3 @@
-import json
 import os
 import random
 import re
@@ -7,11 +6,10 @@ from datetime import UTC, datetime
 
 import yaml
 from flask import Flask, jsonify, render_template, request, session
-from google import genai
-from google.genai import types
 from google.oauth2 import service_account
 
 from config import get_settings
+from llm import evaluate_answer_llm
 from models import Answer, User, Visit, db
 
 SETTINGS = get_settings()
@@ -198,158 +196,6 @@ def evaluate_answer_dummy(claim, argument, counterargument):
         "total_score": total_score,
         "feedback": feedback,
         "overall_feedback": overall_feedback,
-    }
-
-
-def evaluate_answer_llm(question_text, claim, argument, counterargument):
-    """
-    Evaluate the answer using Gemini LLM
-    """
-    client = genai.Client(
-        vertexai=True,
-        credentials=CREDENTIALS,
-        project=SETTINGS.GCLOUD_PROJECT_NAME,
-        location=SETTINGS.GCLOUD_PROJECT_REGION,
-    )
-
-    prompt = f"""
-        Question: {question_text}
-        Claim: {claim}
-        Argument: {argument}
-        Counterargument Rebuttal (Optional): {counterargument}
-    """
-
-    response = client.models.generate_content(
-        model="gemini-exp-1206",
-        contents=[
-            types.Content(
-                role="user",
-                parts=[types.Part.from_text(text=prompt)],
-            ),
-        ],
-        config=types.GenerateContentConfig(
-            temperature=0,  # default: 2
-            top_p=0,  # default: 0.95
-            top_k=1,
-            max_output_tokens=8192,
-            response_modalities=["TEXT"],
-            safety_settings=[
-                types.SafetySetting(
-                    category="HARM_CATEGORY_HATE_SPEECH", threshold="OFF"
-                ),
-                types.SafetySetting(
-                    category="HARM_CATEGORY_DANGEROUS_CONTENT", threshold="OFF"
-                ),
-                types.SafetySetting(
-                    category="HARM_CATEGORY_SEXUALLY_EXPLICIT", threshold="OFF"
-                ),
-                types.SafetySetting(
-                    category="HARM_CATEGORY_HARASSMENT", threshold="OFF"
-                ),
-            ],
-            response_mime_type="application/json",
-            response_schema={
-                "type": "OBJECT",
-                "properties": {
-                    "overall_explanation": {"type": "STRING", "nullable": False},
-                    "overall_rating": {
-                        "type": "INTEGER",
-                        "minimum": 1,
-                        "maximum": 10,
-                        "nullable": False,
-                    },
-                    "clarity_explanation": {"type": "STRING", "nullable": False},
-                    "clarity_rating": {
-                        "type": "INTEGER",
-                        "minimum": 1,
-                        "maximum": 10,
-                        "nullable": False,
-                    },
-                    "logical_structure_explanation": {
-                        "type": "STRING",
-                        "nullable": False,
-                    },
-                    "logical_structure_rating": {
-                        "type": "INTEGER",
-                        "minimum": 1,
-                        "maximum": 10,
-                        "nullable": False,
-                    },
-                    "depth_explanation": {"type": "STRING", "nullable": False},
-                    "depth_rating": {
-                        "type": "INTEGER",
-                        "minimum": 1,
-                        "maximum": 10,
-                        "nullable": False,
-                    },
-                    "objectivity_explanation": {"type": "STRING", "nullable": False},
-                    "objectivity_rating": {
-                        "type": "INTEGER",
-                        "minimum": 1,
-                        "maximum": 10,
-                        "nullable": False,
-                    },
-                    "creativity_explanation": {"type": "STRING", "nullable": False},
-                    "creativity_rating": {
-                        "type": "INTEGER",
-                        "minimum": 1,
-                        "maximum": 10,
-                        "nullable": False,
-                    },
-                },
-                "required": [
-                    "overall_explanation",
-                    "overall_rating",
-                    "clarity_explanation",
-                    "clarity_rating",
-                    "logical_structure_explanation",
-                    "logical_structure_rating",
-                    "depth_explanation",
-                    "depth_rating",
-                    "objectivity_explanation",
-                    "objectivity_rating",
-                    "creativity_explanation",
-                    "creativity_rating",
-                ],
-            },
-            system_instruction=[
-                types.Part.from_text(
-                    text="""You are an argument evaluation system. Arguments always start with a claim and then the reasoning to support the claim.
-                    In the end there might be a section for counterargument rebuttal, but that is optional. Evaluate the argument overall as well as in terms
-                    of the factors clarity, logical structure, depth, objectivity, creativity. Rate from a scale of 1 to 10
-                    and give explanations for each score. Make sure you evaluate arguments rationally.
-                    Claims could potentially be unpopular or sound strange/radical, but if an argument is well-constructed,
-                    it should get a high rating regardless. Return ALL fields in the required JSON format.
-                    Never omit any rating or explanation fields. Use the exact field names from the schema."""
-                )
-            ],
-        ),
-    )
-
-    return parse_llm_response(json.loads(response.text))
-
-
-def parse_llm_response(response):
-    """
-    Convert LLM response format to our evaluation format
-    """
-    return {
-        "scores": {
-            "Logical Structure": response["logical_structure_rating"],
-            "Clarity": response["clarity_rating"],
-            "Depth": response["depth_rating"],
-            "Objectivity": response["objectivity_rating"],
-            "Creativity": response["creativity_rating"],
-        },
-        "total_score": response["overall_rating"],
-        "feedback": {
-            "Logical Structure": response["logical_structure_explanation"],
-            "Clarity": response["clarity_explanation"],
-            "Depth": response["depth_explanation"],
-            "Objectivity": response["objectivity_explanation"],
-            "Creativity": response["creativity_explanation"],
-        },
-        "overall_feedback": response["overall_explanation"],
     }
 
 
