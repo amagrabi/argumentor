91b11249888ec89ca6a9d24792876446af1b4975
amagrabi <amadeus.magrabi@gmail.com>
2025-02-11 13:11:43 +0100

    Extend example answers

--- src/data/example_answers.py
+++ src/data/example_answers.py
@@ -2,3 +2,8 @@ QUESTION_TEXT = """In decision-making, is relying on the bandwagon effect a just
 CLAIM = """Following the bandwagon effect is a justifiable strategy when evidence is scarce."""
 ARGUMENT = """When evidence is scarce, you do not have a lot of reliable options to guide decision-making. Without evidence, you cannot put confidence in the truthfulness of any assumptions. And without a solid foundation, you cannot construct solid arguments. So in a situation when you have no evidence, it would be best to take more time and search evidence. But if you do not have that time and need to make a decision now, it can be better to follow the majority, because it has a higher likelihood of success."""
 COUNTERARGUMENT_REBUTTAL = ""
+
+QUESTION_TEXT = """Should AI be permitted to autonomously make life-or-death decisions (e.g., in automated warfare) if it reduces human casualties?"""
+CLAIM = """AI should be permitted to make important life-or-death decisions."""
+ARGUMENT = """AI has demonstrated super-human intelligence on many domains already. If that is the case, then AI is capable of making better or equal decisions than humans. Therefore, AI should be permitted to make important life-or-death decisions, because there is a high chance that it outperforms humans and can save more lives in the long run."""
+COUNTERARGUMENT_REBUTTAL = ""
